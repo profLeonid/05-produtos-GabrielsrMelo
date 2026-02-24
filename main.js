5252b8fbf0508b7e5182781fdb54025b2178abf0
GABRIEL SOUSA RODRIGUES DE MELO <25203630@dsn127m1242269.cfp127aluno.local>
2026-02-24 11:32:18 -0300

    Ajustando

--- main.js
+++ main.js
@@ -2,10 +2,14 @@
 
 function adicionarProduto(){
     const lista = document.getElementById('lista')
-    const produto = document.getElementById('produto')
+    const produto = document.getElementById('produto') 
+    const codigo = document.getElementById('codigo')
+    const quantidade = document.getElementById('quantidade')
 
     const span = document.createElement('span')
-    span.textContent = produto.value
+    span.textContent = codigo.value
+    span.textContent += produto.value
+    span.textContent += quantidade.value
     span.className = 'flex flex-col bg-yellow-100 px-8 py-2'
 
     lista.appendChild(span)
